006a4ac51620d36e0241b41164419aabca5b54d0
freekvb <freekvb@gmail.com>
2021-10-01 03:14:56 +0200

    suckless

---
+++ Programs/dmenu/config.h
@@ -0,0 +1,26 @@
+/* See LICENSE file for copyright and license details. */
+/* Default settings; can be overriden by command line. */
+
+/* appearance */
+/* -b  option; if 0, dmenu appears at bottom */
+static int topbar = 1;
+/* -fn option overrides fonts[0]; default X11 font or font set */
+static const char *fonts[] = {"Terminus:pixelsize=14", "JoyPixels:pixelsize=10"};
+/* -p  option; prompt to the left of input field */
+static const char *prompt      = " >";
+#include "/home/fvb/.cache/wal/colors-wal-dmenu.h"
+/* static const char *colors[SchemeLast][2] = { */
+	/*     fg         bg       */
+/*	[SchemeNorm] = { "#eeeeee", "#222222" },
+ *   [SchemeSel] = { "#eeeeee", "#555555" },
+ *   [SchemeOut] = { "#000000", "#00ffff" },
+ *};
+ */
+/* -l option; if nonzero, dmenu uses vertical list with given number of lines */
+static unsigned int lines      = 0;
+
+/*
+ * Characters not considered part of a word while deleting words
+ * for example: " /?\"&[]"
+ */
+static const char worddelimiters[] = " ";
