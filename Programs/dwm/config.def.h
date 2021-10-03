359a7811747e028024111ff30e965e7d626d6f1c
freekvb <freekvb@gmail.com>
2021-10-03 20:55:02 +0200

    scratchpad

--- Programs/dwm/config.def.h
+++ Programs/dwm/config.def.h
@@ -59,12 +59,15 @@ static const Layout layouts[] = {
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", NULL };
 static const char *termcmd[]  = { "st", NULL };
+static const char scratchpadname[] = "scratchpad";
+static const char *scratchpadcmd[] = { "st", "-t", scratchpadname, "-g", "120x34", NULL };
 
 static Key keys[] = {
 	/* modifier            key         function           argument */
 
 	{ MODKEY,              XK_space,   spawn,             {.v = dmenucmd } },
 	{ MODKEY,              XK_Return,  spawn,             {.v = termcmd } },
+    { MODKEY,              XK_d,       togglescratch,     {.v = scratchpadcmd } },
 	{ MODKEY,              XK_b,       togglebar,         {0} },
 	{ MODKEY,              XK_j,       focusstack,        {.i = +1 } },
 	{ MODKEY,              XK_k,       focusstack,        {.i = -1 } },
@@ -97,7 +100,6 @@ static Key keys[] = {
     { Mod4Mask,            XK_slash,   spawn,             SHCMD("pamixer -t") },
     { Mod4Mask,            XK_x,       spawn,             SHCMD("mocp -G") },
     { MODKEY,              XK_e,       spawn,             SHCMD("st -e zsh ~/Scripts/emojis") },
-    { MODKEY,              XK_d,       spawn,             SHCMD("dunstify $(date +'%R')") },
     { MODKEY,              XK_v,       spawn,             SHCMD("st -e zsh ~/Scripts/yt_mpv") },
     { MODKEY,              XK_t,       spawn,             SHCMD("scrot '%d-%b-%Y-%H:%M.png' -e 'mv $f ~/Notes/trades/' && dunstify 'chart screenshot  >>  trades'") },
     { MODKEY,              XK_s,       spawn,             SHCMD("scrot '%d-%b-%Y-%H:%M.png' -e 'mv $f ~/Notes/ict/reviews/' && dunstify 'chart screenshot  >>  reviews'") },
