e26dd96795bfb733ae787fd58ab6e0d3a829cf7b
freekvb <freekvb@gmail.com>
2022-03-01 22:15:30 +0100

    user agent

--- .config/qutebrowser/config.py
+++ .config/qutebrowser/config.py
@@ -623,7 +623,7 @@ c.content.cookies.accept = 'no-3rdparty'
 ## User agent to send. Unset to send the default. Note that the value
 ## read from JavaScript is always the global value.
 ## Type: String
-c.content.headers.user_agent = 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/97.0.4692.99 Safari/537.36'
+c.content.headers.user_agent = 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/98.0.4758.102 Safari/537.36'
 
 ## Enable adblocking
 c.content.blocking.method = 'both'
