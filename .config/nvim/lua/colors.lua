df48e23d3bed4369a3b868e8ba38f464f799f5c5
freekvb <freekvb@gmail.com>
2025-04-30 04:14:35 +0200

    general update

--- .config/nvim/lua/colors.lua
+++ .config/nvim/lua/colors.lua
@@ -66,3 +66,25 @@ vim.cmd([[
     hi Error ctermfg=0 ctermbg=1
 ]])
 
+-- diff highlights
+vim.cmd[[
+augroup diffcolors
+    autocmd!
+    autocmd Colorscheme * call s:SetDiffHighlights()
+augroup END
+
+function! s:SetDiffHighlights()
+    if &background == "dark"
+        highlight DiffAdd gui=bold guifg=none guibg=#2e4b2e
+        highlight DiffDelete gui=bold guifg=none guibg=#4c1e15
+        highlight DiffChange gui=bold guifg=none guibg=#45565c
+        highlight DiffText gui=bold guifg=none guibg=#996d74
+    else
+        highlight DiffAdd gui=bold guifg=none guibg=palegreen
+        highlight DiffDelete gui=bold guifg=none guibg=tomato
+        highlight DiffChange gui=bold guifg=none guibg=lightblue
+        highlight DiffText gui=bold guifg=none guibg=lightpink
+    endif
+endfunction
+]]
+
--- .config/zsh/.zshrc
+++ .config/zsh/.zshrc
@@ -70,7 +70,8 @@ zstyle ':fzf-tab:complete:cd:*' fzf-preview 'ls --color $realpath'
 zstyle ':fzf-tab:complete:__zoxide_z:*' fzf-preview 'ls --color $realpath'
 
 # Shell integrations
-eval "$(fzf --zsh)"
+#eval "$(fzf --zsh)"
+source <(fzf --zsh)
 eval "$(zoxide init --cmd cd zsh)"
 
 #}}}
